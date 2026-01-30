ae490c84830f0a346466776c780a082d995cd49e
tigrankhh <khachtik2606@gmail.com>
2026-01-30 17:57:25 +0400

    Refactor getDammeWisdom function for clarity

--- damme-api.js
+++ damme-api.js
@@ -1,16 +1,15 @@
 async function getDammeWisdom() {
     const display = document.getElementById('joke-display');
-    display.innerText = "Connecting to the source of pure power...";
+    display.innerText = "Reaching into the source of power...";
 
     try {
         const response = await fetch('https://api.chucknorris.io/jokes/random');
         const data = await response.json();
-        // The magic replacement
+        // Replace Chuck with the Legend
         let wisdom = data.value.replace(/Chuck Norris/g, "Jean-Claw Van Damme");
         display.innerText = `"${wisdom}"`;
-        
-        if(typeof updateBar === 'function') updateBar(3); // Mental strength gains
+        updateBar(2);
     } catch (e) {
-        display.innerText = "The signal is weak, but my roundhouse kick is strong. Try again.";
+        display.innerText = "The connection is weak, but my spirit is strong. Try again.";
     }
 }
